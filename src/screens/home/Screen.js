5e3da1f90831b4496f734b1382531153ae133246
Luke <greenwood.luke@gmail.com>
2016-04-07 21:18:40 -0700

    MAKIN IT WORK YO

--- src/screens/home/Screen.js
+++ src/screens/home/Screen.js
@@ -1,1 +1,15 @@
-export default { };
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    let header = React.DOM.h2({}, 'Luke Greenwood');
+
+    let subheaders = [
+      React.DOM.h3({}, "The man who does things with stuff"),
+      React.DOM.h3({}, "The ultimate purveyor of glory and truth"),
+      React.DOM.h3({}, "Luke Greenwood")
+    ];
+
+    return React.DOM.div({className: 'center'}, header, ...subheaders);
+  }
+};
